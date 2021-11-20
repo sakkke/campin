d993915b25e9faa3ebbbccdae36e8cd03c447701
sakkke <w32w64@gmail.com>
2021-11-20 12:27:38 +0900

    split file

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,9 @@
 <script>
-  import '@fontsource/bevan'
+  import Heading from './lib/Heading.svelte'
 </script>
 
 <main>
-  <h1>Camp<span>i</span>n</h1>
+  <Heading></Heading>
 </main>
 
-<style>
-  h1 {
-    color: #fff;
-    font-family: Bevan;
-    text-shadow:
-      -1px -1px #000,
-      -1px 1px #000,
-      1px -1px #000,
-      1px 1px #000;
-  }
-
-  h1 span {
-    color: #2bf;
-  }
-</style>
+<style></style>
---
+++ src/lib/Heading.svelte
@@ -0,0 +1,21 @@
+<script>
+  import '@fontsource/bevan'
+</script>
+
+<h1>Camp<span>i</span>n</h1>
+
+<style>
+  h1 {
+    color: #fff;
+    font-family: Bevan;
+    text-shadow:
+      -1px -1px #000,
+      -1px 1px #000,
+      1px -1px #000,
+      1px 1px #000;
+  }
+
+  h1 span {
+    color: #2bf;
+  }
+</style>
