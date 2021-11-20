7b5e04c8849f3656f00248d535a5c54ee3c27d92
sakkke <w32w64@gmail.com>
2021-11-20 12:22:41 +0900

    update heading

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,23 @@
-<script></script>
+<script>
+  import '@fontsource/bevan'
+</script>
 
 <main>
-  <h1>Campin</h1>
+  <h1>Camp<span>i</span>n</h1>
 </main>
 
-<style></style>
+<style>
+  h1 {
+    color: #fff;
+    font-family: Bevan;
+    text-shadow:
+      -1px -1px #000,
+      -1px 1px #000,
+      1px -1px #000,
+      1px 1px #000;
+  }
+
+  h1 span {
+    color: #2bf;
+  }
+</style>
