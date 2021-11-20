201617901e46abc6e14479fe85aebcfb234bd1db
sakkke <w32w64@gmail.com>
2021-11-20 15:41:45 +0900

    add map component

---
+++ src/lib/Map.svelte
@@ -0,0 +1,29 @@
+<script>
+  import L from 'leaflet'
+  import 'leaflet/dist/leaflet.css'
+  import { onMount } from 'svelte'
+
+  export let height = '360px'
+  export let lat = 35.710113 // latitude
+  export let lon = 139.810703 // longitude
+  export let width = 'auto'
+  export let zoom = 18
+
+  const id = `map-${Math.random().toString(36)}`
+
+  onMount(() => {
+    const map = L.map(id, {
+      center: [lat, lon],
+      zoom,
+    })
+    L
+      .tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      })
+      .addTo(map)
+  })
+</script>
+
+<div {id} style="height: {height}; width: {width};"></div>
+
+<style></style>
