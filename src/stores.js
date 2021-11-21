7269a2c2975ed0abcb5cf1fb96923e512440faae
sakkke <w32w64@gmail.com>
2021-11-21 17:33:22 +0900

    make stores persist

--- src/stores.js
+++ src/stores.js
@@ -1,9 +1,15 @@
 import { writable } from 'svelte/store'
 
-export const activeCamp = writable(0)
-export const campingTime = writable(25 * 60 * 1000 /* minutes */)
-export const finishTime = writable(0)
-export const pluscodes = writable([
+function persistentStore (k, v) {
+  const result = writable(JSON.parse(localStorage.getItem(k)) || v)
+  result.subscribe(value => localStorage.setItem(k, JSON.stringify(value)))
+  return result
+}
+
+export const activeCamp = persistentStore('activeCamp', 0)
+export const campingTime = persistentStore('finishTimeUnit', 25 * 60 * 1000 /* minutes */)
+export const finishTime = persistentStore('finishTime', 0)
+export const pluscodes = persistentStore('pluscodes', [
   '8FW4V75V+8Q', // エッフェル塔
   '87G7MXQ4+M5', // 自由の女神
   '8Q7XMQJ8+FR', // 東京駅
